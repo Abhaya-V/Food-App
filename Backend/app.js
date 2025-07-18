8723ad950710ebb3c1611c4d68f730d6de3e9161
Abhaya-V <abhayaviswakumar@gmail.com>
2025-07-18 19:23:57 +0530

    cors deploy

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,12 @@ const sellerRoutes =require("./routes/SellerRoutes")
 const geocodeRoute =require("./utils/geocode")
 const reviewRoutes = require("./routes/reviewRoutes");
 
-app.use(cors());
+
+app.use(cors({
+    origin: "https://food-appfront.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
